7ba8b2e9e842fb684f9504ea0fd6ed18104f3029
denmercs <denmercs@gmail.com>
2020-12-20 23:11:17 -0600

    added rederict in confirmation email once clicked

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -94,7 +94,7 @@ router.get("/confirmation/:token", async (req, res) => {
       { confirmed: true }
     );
 
-    res.status(200).json({ update: confirmedUpdated });
+    res.redirect("/login");
   } catch (err) {
     res.send(err);
   }

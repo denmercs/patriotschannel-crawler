68853efa649a9a876d5896ccb61c9f91a7be6e7e
denmercs <denmercs@gmail.com>
2020-12-30 00:45:42 -0600

    addd categories router

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,11 +3,11 @@ const asyncHandler = require("express-async-handler");
 const News = require("../models/news");
 
 router.get(
-  "/politics",
+  "/trump-pence",
   asyncHandler(async (req, res) => {
     News.createIndexes({ title: "text", content: "text" });
     let categorizedData = await News.find({
-      $text: { $search: "trump", $caseSensitive: false },
+      $text: { $search: "trump, pence", $caseSensitive: false },
     }).sort({ created_at: 1 });
     res.send(categorizedData);
   })
